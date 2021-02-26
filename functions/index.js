fcbc2c18de40657341fb6b1f7f1df9ba39149b18
Robbin Ploeger <robbplo@gmail.com>
2021-02-26 11:49:17 +0100

    Realtime presence and match to queue

--- functions/index.js
+++ functions/index.js
@@ -1,39 +1,61 @@
+require('apiRTC-latest.min');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', {structuredData: true});
-  response.send('Hello from Firebase!');
-});
+const firestore = admin.firestore();
 
-exports.enterQueue = functions.https.onCall(async (data, context) => {
-  const firestore = admin.firestore();
+exports.match = functions.https.onCall(async (data, context) => {
+  const oneHourInMs = (60 * 60 * 6000);
+  const now = new Date;
+  const oneHourAgo = new Date(now.getTime() - oneHourInMs);
 
-  const userIsQueuedSnapshot = await firestore.collection('queue')
-      .where('uid', '==', context.auth.uid)
-      .limit(1)
+  const queuedUsers = await firestore.collection('status')
+      .where('last_changed', '>', oneHourAgo)
+      .where('state', '==', 'online')
+      .orderBy('last_changed')
+      .limit(2)
       .get();
 
+  const selfExcluded = queuedUsers.docs
+      .filter((user) => user.id !== context.auth.uid);
 
-  if (userIsQueuedSnapshot.empty) {
-    await firestore.collection('queue').add({
-      uid: context.auth.uid,
-    });
+  // @todo exclude disliked users
+
+  if (selfExcluded.length === 0) {
+    // @todo maybe this should throw an error instead.
+    return {
+      matched: false,
+      message: 'Could not match: no queued users',
+    };
   }
+
+  const recipientUid = selfExcluded[0].id;
+
+  await firestore.collection('conversations').doc(context.auth.uid).set({
+    connectionString: 'iets',
+    recipient: recipientUid,
+    timestamp: new Date,
+  });
+
+  await firestore.collection('conversations').doc(recipientUid).set({
+    connectionString: 'iets anders',
+    recipient: context.auth.uid,
+    timestamp: new Date,
+  });
+
+
+  return {
+    matched: true,
+    uid: recipientUid,
+  };
 });
 
 exports.userSaver = functions.auth.user().onCreate(async (user, ctx) => {
-  const firestore = admin.firestore();
-
-  // Make a document in the user's collection with everything we know about the
-  // user
+  // Make a document in the user's collection with everything we know about them
   const userId = user.uid;
   const userRef = firestore.collection('users').doc(userId);
-  console.log(user.toJSON());
+
   await userRef.set({
     uid: userId,
     email: user.email,
@@ -41,4 +63,53 @@ exports.userSaver = functions.auth.user().onCreate(async (user, ctx) => {
     score: 0,
     created_at: user.metadata.creationTime,
   });
+
+
+  ua.register({
+
+  });
 });
+
+exports.onUserStatusChanged = functions.database.ref('/status/{uid}').onUpdate(
+    async (change, context) => {
+    // Get the data written to Realtime Database
+      const eventStatus = change.after.val();
+
+      // Then use other event data to create a reference to the
+      // corresponding Firestore document.
+      const userStatusFirestoreRef = firestore
+          .doc(`status/${context.params.uid}`);
+
+      // It is likely that the Realtime Database change that triggered
+      // this event has already been overwritten by a fast change in
+      // online / offline status, so we'll re-read the current data
+      // and compare the timestamps.
+      const statusSnapshot = await change.after.ref.once('value');
+      const status = statusSnapshot.val();
+
+      // If the current timestamp for this data is newer than
+      // the data that triggered this event, we exit this function.
+      if (status.last_changed > eventStatus.last_changed) {
+        return null;
+      }
+
+      // Otherwise, we convert the last_changed field to a Date
+      eventStatus.last_changed = new Date(eventStatus.last_changed);
+
+      // ... and write it to Firestore.
+      await userStatusFirestoreRef.set(eventStatus);
+
+      // Then clear any outstanding conversations for the user and the
+      // recipient conversation
+      const conversationRef = firestore.collection('conversations')
+          .doc(context.auth.uid);
+
+      const conversation = await conversationRef.get();
+      if (conversation.exists) {
+        const recipient = conversation.data().recipient;
+        console.log(recipient);
+
+        await firestore.collection('conversations').doc(recipient).delete();
+        await conversationRef.delete();
+      }
+    });
--- public/index.js
+++ public/index.js
@@ -17,20 +17,85 @@
 // Initialze Firebase pointing at our test project
 firebase.initializeApp({
   projectId: "secondes-2d06a",
-  apiKey: "fakeApiKey"
+  apiKey: "fakeApiKey",
 });
-debugger
 
-const db = firebase.firestore();
+const firestore = firebase.firestore();
+const database = firebase.database();
 const auth = firebase.auth();
 const functions = firebase.functions();
 
 auth.useEmulator("http://localhost:9099");
 functions.useEmulator("localhost", 5001)
-db.useEmulator("localhost", 8080);
+firestore.useEmulator("localhost", 8080);
+database.useEmulator("localhost", 9098)
 
+const match = functions.httpsCallable('match');
 
-const enterQueue = functions.httpsCallable('enterQueue');
+function detectPresence() {
+  console.log("Detecting presence")
+
+// Fetch the current user's ID from Firebase Authentication.
+  var uid = auth.currentUser.uid;
+
+// Create a reference to this user's specific status node.
+// This is where we will store data about being online/offline.
+  var userStatusDatabaseRef = database.ref('/status/' + uid);
+  var userStatusFirestoreRef = firestore.doc('/status/' + uid);
+
+// We'll create two constants which we will write to
+// the Realtime database when this device is offline
+// or online.
+  var isOfflineForDatabase = {
+    state: 'offline',
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+  };
+
+  var isOnlineForDatabase = {
+    state: 'online',
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+  };
+
+  var isOfflineForFirestore = {
+    state: 'offline',
+    last_changed: firebase.firestore.FieldValue.serverTimestamp(),
+  };
+
+  var isOnlineForFirestore = {
+    state: 'online',
+    last_changed: firebase.firestore.FieldValue.serverTimestamp(),
+  };
+
+// Create a reference to the special '.info/connected' path in
+// Realtime Database. This path returns `true` when connected
+// and `false` when disconnected.
+  database.ref('.info/connected').on('value', function (snapshot) {
+    console.log(snapshot.val())
+    // If we're not currently connected, don't do anything.
+    if (snapshot.val() === false) {
+      userStatusFirestoreRef.set(isOfflineForFirestore);
+
+      return;
+    }
+
+    // If we are currently connected, then use the 'onDisconnect()'
+    // method to add a set which will only trigger once this
+    // client has disconnected by closing the app,
+    // losing internet, or any other means.
+    userStatusDatabaseRef.onDisconnect().set(isOfflineForDatabase).then(function () {
+      // The promise returned from .onDisconnect().set() will
+      // resolve as soon as the server acknowledges the onDisconnect()
+      // request, NOT once we've actually disconnected:
+      // https://firebase.google.com/docs/reference/js/database.OnDisconnect
+
+      // We can now safely set ourselves as 'online' knowing that the
+      // server will mark us as offline once we lose connection.
+      userStatusDatabaseRef.set(isOnlineForDatabase);
+      userStatusFirestoreRef.set(isOnlineForFirestore);
+    });
+  });
+}
+
 
 // Use Vue.js to populate the UI with data
 //
@@ -40,16 +105,19 @@ const app = new Vue({
   el: "#app",
   data: {
     currentUser: null,
-    queue: [],
+    onlineUsers: [],
+    matchResponse: {},
+    conversation: null,
     msgInput: "",
     emailInput: "robbin@smit.net",
     passwordInput: "password",
   },
   methods: {
-    async enterQueue() {
-      console.log(await enterQueue({ma:'nd'}));
+    async match() {
+
+      this.matchResponse = await match()
     },
-    signUp: async function() {
+    signUp: async function () {
       console.log("Attempting sign up as", this.emailInput);
       try {
         const user = await auth.createUserWithEmailAndPassword(this.emailInput, this.passwordInput);
@@ -58,7 +126,7 @@ const app = new Vue({
         console.warn(e);
       }
     },
-    signIn: async function() {
+    signIn: async function () {
       console.log("Attempting sign in as", this.emailInput);
       try {
         const user = await auth.signInWithEmailAndPassword(this.emailInput, this.passwordInput);
@@ -72,12 +140,25 @@ const app = new Vue({
       if (user != null) {
         console.log("Signed in as ", user);
 
-        // Listen to the queue collection
-        db.collection("queue")
+        detectPresence()
+
+        // Update online users
+        firestore.collection("status")
           .onSnapshot((snap) => {
-            console.log("Got data from firestore!");
-            this.queue = snap.docs.map((doc) => doc.data());
-          });
+            this.onlineUsers = snap.docs.map(doc => ({
+              uid: doc.id,
+              ...doc.data()
+            })).filter(user => user.state === 'online')
+          })
+        firestore.collection("conversations")
+          .doc(user.uid)
+          .onSnapshot(snap => {
+            if (snap.exists) {
+              this.conversation = snap.data()
+            } else {
+              this.conversation = null
+            }
+          })
       }
     }
   },
